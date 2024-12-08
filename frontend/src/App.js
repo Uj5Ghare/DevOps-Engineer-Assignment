bcab7aec84be660dcb8f83966ab940d403c1a8d6
Uj5Ghare <ujwal5ghare@gmail.com>
2024-12-08 07:28:30 +0000

    revert(commit): feat(connection) Added connection between frotend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,22 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
-import React, { useState, useEffect } from 'react';
-
 function App() {
-  const [apiResponse, setApiResponse] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8000/users');
-        const data = await response.text();
-        setApiResponse(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>{apiResponse}</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Developer:
+        </p>
+        <a
+          className="App-link"
+          href="https://uj5ghare.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ujwal Pachghare
+        </a>
       </header>
     </div>
   );
